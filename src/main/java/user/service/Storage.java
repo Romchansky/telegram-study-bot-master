59e4f3a89b72144f9667d9a0de71dab7410ef3d7
PayneMaxPayne <75696288+PayneMaxPayne@users.noreply.github.com>
2021-06-08 16:03:11 +0300

    Update Storage.java

--- src/main/java/user/service/Storage.java
+++ src/main/java/user/service/Storage.java
@@ -5,4 +5,5 @@ import org.telegram.telegrambots.meta.api.objects.User;
 public interface Storage {
      void putUserSettings(UserSettings userSettings);
      UserSettings getUserByChatId(Long chatId);
+     boolean validateEmail(String email);
 }
