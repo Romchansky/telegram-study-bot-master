72febe6720fe798fe796815ff664bc5448081f4b
UAValchyMa <mvalchyshen@gmail.com>
2021-06-08 14:29:54 +0300

    UserSettings 0.1

---
+++ src/main/java/user/service/Storage.java
@@ -0,0 +1,8 @@
+package user.service;
+
+import org.telegram.telegrambots.meta.api.objects.User;
+
+public interface Storage {
+     void putUserSettings(UserSettings userSettings);
+     UserSettings getUserByChatId(Long chatId);
+}
